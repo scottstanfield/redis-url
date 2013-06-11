561c8f005a927f339e488857aa699cfe922cf2d2
Scott Stanfield <scott@vertigo.com>
2013-06-11 00:40:16 -0700

    Using newer REDISTOGO_URL

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var url = require('url');
 
 module.exports.createClient = module.exports.connect = function(redis_url, options) {
   var password, database;
-  var parsed_url  = url.parse(redis_url || process.env.REDIS_URL || 'redis://localhost:6379');
+  var parsed_url  = url.parse(redis_url || process.env.REDISTOGO_URL || 'redis://localhost:6379');
   var parsed_auth = (parsed_url.auth || '').split(':');
 
   var redis = require('redis').createClient(parsed_url.port, parsed_url.hostname, options);
